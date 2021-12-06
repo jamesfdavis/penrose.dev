e847abea6e7aa67777f8f31e55cc4d314ecea91d
James Davis <ragingsmurf@gmail.com>
2021-12-06 12:46:20 -0700

    TailWind basic layout details.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,11 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="p-6 sm:p-10 md:p-16 flex flex-wrap">
+	<slot />
+</div>
+
+<!-- Pin to top right corner -->
+<!-- <div class="absolute top-0 right-0 h-12 w-18 p-4 none">
+	<button class="js-change-theme focus:outline-none">🌙</button>
+</div> -->
