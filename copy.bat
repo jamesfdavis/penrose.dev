01dce45a9f3cd27c72575f9bc6cca04641472ba3
James Davis <ragingsmurf@gmail.com>
2022-05-05 10:42:06 -0600

    Svelte init

--- copy.bat
+++
@@ -1,3 +0,0 @@
-echo "Copied md files to project gen/md"
-echo "run npm dev"
-cp -R $LOCAL_PENROSE ./gen/md
--- src/content/subtitle.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	export let subtitle = 'Magic Valley Software Solutions';
-</script>
-
-<slot>
-	<h2 class="text-black-800 text-lg font-design">{subtitle}</h2>
-</slot>
