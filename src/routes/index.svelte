154408110c7e62987525d048e812e455b7dfd5de
James Davis <ragingsmurf@gmail.com>
2022-05-08 10:19:47 -0600

    Fresh build

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,60 @@
-<div class="md:p-20 pt-20 pl-2">
-	<h2 class="font-semibold text-black pt-3 text-xl">James F. Davis</h2>
-	<h2 class=" text-black mb-3">Penrose Development</h2>
-	<ul id="links" class="list-none prose pl-0 text-sm">
-		<li class="m-0 pl-0">
-			<a href="/blog">Blog</a>
-		</li>
-		<li class="m-0 pl-0">
-			<a href="/">Projects</a>
-		</li>
-		<li class="m-0 pl-0">
-			<a href="http://www.github.com/jamesfdavis" target="_blank">Code</a>
-		</li>
-		<li class="mt-5 pl-0">jim [at] penrose.dev</li>
-	</ul>
-</div>
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
